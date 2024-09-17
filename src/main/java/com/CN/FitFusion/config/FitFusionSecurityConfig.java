8e087fc4289c730beeb54a36ec80904e49ad2feb
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 18:14:45 +0530

    adding authenticationManager bean

--- src/main/java/com/CN/FitFusion/config/FitFusionSecurityConfig.java
+++ src/main/java/com/CN/FitFusion/config/FitFusionSecurityConfig.java
@@ -3,6 +3,8 @@ package com.CN.FitFusion.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,6 +37,11 @@ public class FitFusionSecurityConfig {
         http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
+        return builder.getAuthenticationManager();
     }
 
     @Bean
