3850e9de38bb75751cfc974aad432e14145da5e1
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 14:37:27 +0530

    adding getClaimsFromToken method in JwtAuthenticationHelper

--- src/main/java/com/CN/FitFusion/jwt/JwtAuthenticationHelper.java
+++ src/main/java/com/CN/FitFusion/jwt/JwtAuthenticationHelper.java
@@ -3,9 +3,20 @@ package com.CN.FitFusion.jwt;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 @Component
 public class JwtAuthenticationHelper {
+
+    private final static String TOKEN_SECRE_STRING = "thisisacodingninjasdemonstrationforsecretkeyinspringsecurityjsonwebtokenauthentication";
     
-    
+    public Claims getClaimsFromToken(String token){
+        Claims claims = Jwts.parserBuilder()
+                            .setSigningKey(TOKEN_SECRE_STRING.getBytes())
+                            .build()
+                            .parseClaimsJws(token)
+                            .getBody();
+
+            return claims;
+    }
 }
