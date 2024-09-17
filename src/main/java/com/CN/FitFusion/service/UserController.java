e68c2ccf9addb7836a11986030248ceeacdb8770
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 17:34:55 +0530

    adding registerUser method in UserController

--- src/main/java/com/CN/FitFusion/service/UserController.java
+++ src/main/java/com/CN/FitFusion/service/UserController.java
@@ -1,10 +1,25 @@
 package com.CN.FitFusion.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CN.FitFusion.dto.UserDto;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerUser(@RequestBody UserDto userDto){
+        userService.createUser(userDto);
+    }
 }
