5313990c790c8c7de6f8cd2bb02d10bec478c9aa
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 17:31:03 +0530

    adding getAllUsers method in UserService

--- src/main/java/com/CN/FitFusion/service/UserService.java
+++ src/main/java/com/CN/FitFusion/service/UserService.java
@@ -1,6 +1,7 @@
 package com.CN.FitFusion.service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,5 +52,9 @@ public class UserService {
             user.setRoles(roles);
         }
         userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
