b61edca0ad7b526ae54eb1e9c3909575b69f62d0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 14:43:06 +0530

    getting username from jwt helper in jwtauth filter

--- src/main/java/com/CN/FitFusion/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/CN/FitFusion/jwt/JwtAuthenticationFilter.java
@@ -7,22 +7,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter{
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtAuthenticationHelper jwtHelper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        
-            String headers = request.getHeader("authorization");
 
-            if (headers != null && headers.startsWith("Bearer")) {
-                    
-                    String token = headers.split(" ")[1];
-            }
+        String headers = request.getHeader("authorization");
+
+        if (headers != null && headers.startsWith("Bearer")) {
+
+            String token = headers.split(" ")[1];
+
+            String username = jwtHelper.getUserNameFromToken(token);
+        }
     }
-    
+
 }
