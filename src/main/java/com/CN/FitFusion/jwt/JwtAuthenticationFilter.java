f581031ff025ebdc0795eb85e2e9096c27c22a01
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 14:29:55 +0530

    implementing doFilterInternal in JwtAuthenticationFilter

--- src/main/java/com/CN/FitFusion/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/CN/FitFusion/jwt/JwtAuthenticationFilter.java
@@ -1,8 +1,28 @@
 package com.CN.FitFusion.jwt;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+            String headers = request.getHeader("authorization");
+
+            if (headers != null && headers.startsWith("Bearer")) {
+                    
+                    String token = headers.split(" ")[1];
+            }
+    }
     
 }
