96fe800333b3716ffced45ab7de617604c6ea8b2
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 15:57:34 +0530

    adding doAuthenticate method in AuthService

--- src/main/java/com/CN/FitFusion/service/AuthService.java
+++ src/main/java/com/CN/FitFusion/service/AuthService.java
@@ -1,8 +1,24 @@
 package com.CN.FitFusion.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    @Autowired
+    private AuthenticationManager manager;
     
+    public void doAuthenticate(String username, String password){
+        UsernamePasswordAuthenticationToken upat = 
+            new UsernamePasswordAuthenticationToken(username,password);
+        try {
+            manager.authenticate(upat);
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+    }
 }
