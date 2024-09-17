fab43967e6b8ec5d8727f24aa2c30aa7dfa99941
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 15:05:48 +0530

    adding findByEmail method in UserRepository

--- src/main/java/com/CN/FitFusion/repository/UserRepository.java
+++ src/main/java/com/CN/FitFusion/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.CN.FitFusion.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.CN.FitFusion.model.User;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
     
+    Optional<User> findByEmail(String email);
 }
