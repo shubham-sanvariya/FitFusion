e57e5a378820db30abd2c9b00306eb6a664e664d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 12:21:40 +0530

    adding manytomany mapping with role entity

--- src/main/java/com/CN/FitFusion/model/User.java
+++ src/main/java/com/CN/FitFusion/model/User.java
@@ -1,9 +1,17 @@
 package com.CN.FitFusion.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -31,7 +39,11 @@ public class User {
 
     private Long contactNo;
 
-    // Set< Role > roles (ManyToMany)
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+     joinColumns =  @JoinColumn(name = "user",referencedColumnName = "id"),
+     inverseJoinColumns = @JoinColumn(name = "role", referencedColumnName = "id"))
+    private Set< Role > roles = new HashSet<>();
 
     // List< Exercise > exerciseList (OneToMany)
 
