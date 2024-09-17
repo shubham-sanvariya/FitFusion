16c2dc142ae772eec26c357e4cd2ddfa87e79324
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 12:25:19 +0530

    adding field properties for Exercise Entity

--- src/main/java/com/CN/FitFusion/model/Exercise.java
+++ src/main/java/com/CN/FitFusion/model/Exercise.java
@@ -1,8 +1,32 @@
 package com.CN.FitFusion.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private int sets;
     
+    private int reps;
+
+    // User user (ManyToOne)
 }
