d2d4b4d8148d757c9bb4be87c5a765d1365e2fb9
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 15:13:17 +0530

    adding loadUserByUsername method in CustomUserDetailService

--- src/main/java/com/CN/FitFusion/security/CustomUserDetailService.java
+++ src/main/java/com/CN/FitFusion/security/CustomUserDetailService.java
@@ -1,8 +1,24 @@
 package com.CN.FitFusion.security;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.CN.FitFusion.repository.UserRepository;
+
 @Service
-public class CustomUserDetailService {
+public class CustomUserDetailService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
     
 }
