aa61d8119d4bb127217dcc7be6b43236466cd5f2
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-17 13:04:39 +0530

    adding field properties in Diet entity

--- src/main/java/com/CN/FitFusion/model/Diet.java
+++ src/main/java/com/CN/FitFusion/model/Diet.java
@@ -1,8 +1,29 @@
 package com.CN.FitFusion.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Diet {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    // private User user;
 }
