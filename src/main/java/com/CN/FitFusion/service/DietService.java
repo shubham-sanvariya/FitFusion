12a86cb20286081eb1cccd72cf1cf321d7611df7
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 16:58:43 +0530

    adding getDietById method in DietService

--- src/main/java/com/CN/FitFusion/service/DietService.java
+++ src/main/java/com/CN/FitFusion/service/DietService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.CN.FitFusion.exception.DietNotFoundException;
 import com.CN.FitFusion.model.Diet;
 import com.CN.FitFusion.repository.DietRepository;
 
@@ -16,5 +17,10 @@ public class DietService {
 
     public List<Diet> getAllDiets(){
         return dietRepository.findAll();
+    }
+
+    public Diet getDietById(Long id){
+        return dietRepository.findById(id)
+            .orElseThrow(() -> new DietNotFoundException("diet not found"));
     }
 }
