0270d9642e9fb4a921ed33ee6388228e5d41c64d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 14:53:20 +0530

    adding getAllExercises method in ExerciseService

--- src/main/java/com/CN/FitFusion/service/ExerciseService.java
+++ src/main/java/com/CN/FitFusion/service/ExerciseService.java
@@ -1,8 +1,20 @@
 package com.CN.FitFusion.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.CN.FitFusion.model.Exercise;
+import com.CN.FitFusion.repository.ExerciseRepository;
 
 @Service
 public class ExerciseService {
     
+    @Autowired
+    private ExerciseRepository exerciseRepository;
+
+    public List<Exercise> getAllExercises(){
+        return exerciseRepository.findAll();
+    }
 }
