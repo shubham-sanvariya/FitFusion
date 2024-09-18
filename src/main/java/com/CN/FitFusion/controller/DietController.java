e3141a36d1f27af0cbf06f111deb367dd406e6eb
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 17:15:43 +0530

    adding deleteDietById method mapping in DietController

--- src/main/java/com/CN/FitFusion/controller/DietController.java
+++ src/main/java/com/CN/FitFusion/controller/DietController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.CN.FitFusion.dto.DietDto;
-import com.CN.FitFusion.dto.ExerciseDto;
 import com.CN.FitFusion.model.Diet;
-import com.CN.FitFusion.model.Exercise;
 import com.CN.FitFusion.service.DietService;
 
 @RestController
@@ -53,5 +52,12 @@ public class DietController {
     @ResponseStatus(HttpStatus.OK)
     public void updateDiet(@RequestBody DietDto dietDto, @PathVariable Long id){
         dietService.updateDiet(dietDto,id);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('TRAINER')")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteDietById(@PathVariable Long id){
+        dietService.deleteDietById(id);
     }
 }
