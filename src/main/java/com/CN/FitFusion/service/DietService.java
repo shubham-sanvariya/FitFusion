9911328cccee4f10bf4c5c4ccf65dcd5e7e7a24a
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 16:48:21 +0530

    adding getAllDiets in DietService

--- src/main/java/com/CN/FitFusion/service/DietService.java
+++ src/main/java/com/CN/FitFusion/service/DietService.java
@@ -1,8 +1,20 @@
 package com.CN.FitFusion.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.CN.FitFusion.model.Diet;
+import com.CN.FitFusion.repository.DietRepository;
 
 @Service
 public class DietService {
     
+    @Autowired
+    private DietRepository dietRepository;
+
+    public List<Diet> getAllDiets(){
+        return dietRepository.findAll();
+    }
 }
