3ef4d6e525b8e31a26ffba9df2baad808cb36b83
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 17:03:44 +0530

    adding addDietForUser method in DietService

--- src/main/java/com/CN/FitFusion/service/DietService.java
+++ src/main/java/com/CN/FitFusion/service/DietService.java
@@ -5,15 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.CN.FitFusion.dto.DietDto;
 import com.CN.FitFusion.exception.DietNotFoundException;
+import com.CN.FitFusion.exception.UserNotFoundException;
 import com.CN.FitFusion.model.Diet;
+import com.CN.FitFusion.model.User;
 import com.CN.FitFusion.repository.DietRepository;
+import com.CN.FitFusion.repository.UserRepository;
 
 @Service
 public class DietService {
     
     @Autowired
     private DietRepository dietRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public List<Diet> getAllDiets(){
         return dietRepository.findAll();
@@ -22,5 +29,21 @@ public class DietService {
     public Diet getDietById(Long id){
         return dietRepository.findById(id)
             .orElseThrow(() -> new DietNotFoundException("diet not found"));
+    }
+
+    public void addDietForUser(DietDto dietDto, Long userId){
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UserNotFoundException("user not found"));
+
+        Diet diet =  Diet.builder()
+                        .name(dietDto.getName())
+                        .description(dietDto.getDescription())
+                        .build();
+        
+        user.getDiets().add(diet);
+        diet.setUser(user);
+
+        dietRepository.save(diet);
+        userRepository.save(user);
     }
 }
