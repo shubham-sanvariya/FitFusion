102b5f3d446e6aca11041f5ce05e56693bde63c3
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 15:27:34 +0530

    adding ExerciseController

---
+++ src/main/java/com/CN/FitFusion/controller/ExerciseController.java
@@ -0,0 +1,10 @@
+package com.CN.FitFusion.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/diet")
+public class ExerciseController {
+    
+}
