89282221bb3a2f7bd98e31df5869df33677ec27d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 14:17:24 +0530

    adding mapping deleteUserById for User Controller

--- src/main/java/com/CN/FitFusion/controller/UserController.java
+++ src/main/java/com/CN/FitFusion/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,6 +52,13 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public void updateUser(@PathVariable Long id, @RequestBody UserDto userDto){
         userService.updateUser(userDto,id);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteUserById(@PathVariable Long id){
+        userService.deleteUserById(id);
     }
     
 }
