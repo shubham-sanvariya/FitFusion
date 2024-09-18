fba525061d5f0552b8b83812654e24d409cfca24
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 15:55:33 +0530

    adding updateExercise mapping in ExerciseController

--- src/main/java/com/CN/FitFusion/controller/ExerciseController.java
+++ src/main/java/com/CN/FitFusion/controller/ExerciseController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,11 +39,17 @@ public class ExerciseController {
         return exerciseService.getExercisesById(id);
     }
 
-    @GetMapping("/create/{userId}")
+    @PostMapping("/create/{userId}")
     @PreAuthorize("hasRole('TRAINER')")
     @ResponseStatus(HttpStatus.OK)
     public void createExerciseForUser(@RequestBody ExerciseDto exerciseDto, @PathVariable Long userId){
         exerciseService.addExerciseForUser(exerciseDto,userId);
     }
 
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('TRAINER')")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateExercise(@RequestBody ExerciseDto exerciseDto, @PathVariable Long id){
+        exerciseService.updateExercise(exerciseDto,id);
+    }
 }
