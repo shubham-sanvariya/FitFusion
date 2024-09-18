66cd3e7241598557b86a05ee05ef4982fbba5cea
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 17:09:14 +0530

    adding getAllDiets method mapping in DietController

--- src/main/java/com/CN/FitFusion/controller/DietController.java
+++ src/main/java/com/CN/FitFusion/controller/DietController.java
@@ -1,10 +1,29 @@
 package com.CN.FitFusion.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.CN.FitFusion.model.Diet;
+import com.CN.FitFusion.service.DietService;
 
 @RestController
 @RequestMapping("/diet")
 public class DietController {
     
+    @Autowired
+    private DietService dietService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('TRAINER')")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Diet> getAllDiets(){
+        return dietService.getAllDiets();
+    }
 }
