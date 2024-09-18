cd20434db87d460d1990676c9122e7c742675aec
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-18 15:07:34 +0530

    adding addExerciseForUser method in ExerciseService

--- src/main/java/com/CN/FitFusion/service/ExerciseService.java
+++ src/main/java/com/CN/FitFusion/service/ExerciseService.java
@@ -5,15 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.CN.FitFusion.dto.ExerciseDto;
 import com.CN.FitFusion.exception.ExerciseNotFoundException;
+import com.CN.FitFusion.exception.UserNotFoundException;
 import com.CN.FitFusion.model.Exercise;
+import com.CN.FitFusion.model.User;
 import com.CN.FitFusion.repository.ExerciseRepository;
+import com.CN.FitFusion.repository.UserRepository;
 
 @Service
 public class ExerciseService {
     
     @Autowired
     private ExerciseRepository exerciseRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public List<Exercise> getAllExercises(){
         return exerciseRepository.findAll();
@@ -22,5 +29,19 @@ public class ExerciseService {
     public Exercise getExercisesById(Long id){
         return exerciseRepository.findById(id)
             .orElseThrow(() -> new ExerciseNotFoundException("exercise not found"));
+    }
+
+    public void addExerciseForUser(ExerciseDto exerciseDto, Long userId){
+        User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new UserNotFoundException("user not found"));
+
+        Exercise exercise = Exercise.builder().name(exerciseDto.getName())
+                            .description(exerciseDto.getDescription())
+                            .sets(exerciseDto.getSets())
+                            .reps(exerciseDto.getReps())
+                            .build();
+        user.getExerciseList().add(exercise);
+        exerciseRepository.save(exercise);
+        userRepository.save(user);
     }
 }
